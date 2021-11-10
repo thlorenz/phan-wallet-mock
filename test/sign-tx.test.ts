e345e1c1ba0f3e2ca39df8f88d6ad984c41a2e63
Thorsten Lorenz <thlorenz@gmx.de>
2021-11-10 10:34:53 -0600

    test: multi tx and connection tests

--- test/sign-tx.test.ts
+++ test/sign-tx.test.ts
@@ -1,29 +1,17 @@
 import {
   Keypair,
   LAMPORTS_PER_SOL,
+  RpcResponseAndContext,
+  SignatureResult,
   SystemProgram,
   Transaction,
   TransactionInstruction,
   TransactionSignature,
 } from '@solana/web3.js'
 import { createWalletMock } from '../src/phan-wallet-mock'
-import { createAccount, LOCALNET } from './utils'
+import { createAccount, isBuffer, LOCALNET } from './utils'
 import spok from 'spok'
 import test from 'tape'
-import * as util from 'util'
-
-function inspect(x: any) {
-  return util.inspect(x, { depth: 5 })
-}
-
-// @ts-ignore used once in a while
-function dump(x: any) {
-  console.log(inspect(x))
-}
-
-function isBuffer(x: any): x is Buffer {
-  return Buffer.isBuffer(x)
-}
 
 function trimTransferIx(
   ix: TransactionInstruction
@@ -109,6 +97,109 @@ test('sign: transaction with one transfer instruction', async (t) => {
     const res = await wallet.connection.confirmTransaction(sig)
     spok(t, res, {
       $topic: 'tx:confirmed',
+      context: { slot: spok.gtz },
+      value: { err: spok.notDefined },
+    })
+  } catch (err: any) {
+    t.fail(err)
+  }
+})
+
+test('sign: multiple transactions with one transfer instruction each', async (t) => {
+  try {
+    const { wallet, payer } = await setup()
+    const publicKey = payer.publicKey
+    let sig: TransactionSignature
+    let res: RpcResponseAndContext<SignatureResult>
+
+    // -----------------
+    // Create Receiver Account
+    // -----------------
+    const receiver = Keypair.generate()
+    sig = await createAccount(wallet.connection, payer, receiver)
+    t.comment(`+++ Created account, signature: ${sig} +++`)
+
+    // -----------------
+    // Create Transfer Ix and Tx
+    // -----------------
+    const transferTxUno = new Transaction().add(
+      SystemProgram.transfer({
+        lamports: 10,
+        fromPubkey: publicKey,
+        toPubkey: receiver.publicKey,
+      })
+    )
+
+    const transferTxDos = new Transaction().add(
+      SystemProgram.transfer({
+        lamports: 11,
+        fromPubkey: publicKey,
+        toPubkey: receiver.publicKey,
+      })
+    )
+
+    // -----------------
+    // Sign Transfer Txs
+    // -----------------
+    const [signedTransferTxUno, signedTransferTxDos] =
+      await wallet.signAllTransactions([transferTxUno, transferTxDos])
+
+    spok(t, signedTransferTxUno, {
+      $topic: 'txuno:signed',
+      signatures: [{ signature: isBuffer, publicKey }],
+      feePayer: spok.notDefined,
+      instructions: [
+        trimTransferIx(
+          SystemProgram.transfer({
+            lamports: 10,
+            fromPubkey: publicKey,
+            toPubkey: receiver.publicKey,
+          })
+        ),
+      ],
+      recentBlockhash: spok.string,
+      nonceInfo: spok.notDefined,
+    })
+    spok(t, signedTransferTxUno, {
+      $topic: 'txdos:signed',
+      signatures: [{ signature: isBuffer, publicKey }],
+      feePayer: spok.notDefined,
+      instructions: [
+        trimTransferIx(
+          SystemProgram.transfer({
+            lamports: 10,
+            fromPubkey: publicKey,
+            toPubkey: receiver.publicKey,
+          })
+        ),
+      ],
+      recentBlockhash: spok.string,
+      nonceInfo: spok.notDefined,
+    })
+
+    // -----------------
+    // Send and Confirm Transfer Tx
+    // -----------------
+    sig = await wallet.connection.sendRawTransaction(
+      signedTransferTxUno.serialize()
+    )
+    spok(t, { sig }, { $topic: 'txuno:ssent', sig: spok.string })
+
+    res = await wallet.connection.confirmTransaction(sig)
+    spok(t, res, {
+      $topic: 'txuno:confirmed',
+      context: { slot: spok.gtz },
+      value: { err: spok.notDefined },
+    })
+
+    sig = await wallet.connection.sendRawTransaction(
+      signedTransferTxDos.serialize()
+    )
+    spok(t, { sig }, { $topic: 'txdos:ssent', sig: spok.string })
+
+    res = await wallet.connection.confirmTransaction(sig)
+    spok(t, res, {
+      $topic: 'txdos:confirmed',
       context: { slot: spok.gtz },
       value: { err: spok.notDefined },
     })
